73f5cd9ef21be82c2ba6ad981881792e73349896
TamasSallai <tamas.sallai.hu@gmail.com>
2022-10-20 17:05:42 +0200

    addd mongo.js file for db connection, with mongoose

---
+++ phonebook-backend/mongo.js
@@ -0,0 +1,63 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log(
+    'Please provide the password, and optionally the name and the number as an argument: node mongo.js <password> <name> <number>'
+  )
+  process.exit(1)
+}
+
+if (process.argv.length === 4) {
+  console.log(
+    'Please provide a number for the name as an argument: node mongo.js <password> <name> <number>'
+  )
+  process.exit(1)
+}
+
+const url = `mongodb+srv://fullstackopen2022:${process.argv[2]}@cluster0.89srk3t.mongodb.net/phonebook?retryWrites=true&w=majority`
+
+const personSchema = mongoose.Schema({
+  name: String,
+  number: String,
+})
+const Person = mongoose.model('Person', personSchema)
+
+const addPerson = (name, number) => {
+  mongoose
+    .connect(url)
+    .then(() => {
+      console.log('connected')
+      const person = new Person({
+        name: name,
+        number: number,
+      })
+      return person.save()
+    })
+    .then((result) => {
+      console.log(`added ${result.name} ${result.number} to phonebook`)
+      mongoose.connection.close()
+    })
+    .catch((err) => console.log(err))
+}
+
+const getAllPersons = () => {
+  mongoose
+    .connect(url)
+    .then(() => Person.find({}))
+    .then((persons) => {
+      console.log('phonebook:')
+      persons.forEach((person) =>
+        console.log(`${person.name} ${person.number}`)
+      )
+      mongoose.connection.close()
+    })
+    .catch((err) => console.log(err))
+}
+
+if (process.argv.length === 3) {
+  getAllPersons()
+}
+
+if (process.argv.length === 5) {
+  addPerson(process.argv[3], process.argv[4])
+}
