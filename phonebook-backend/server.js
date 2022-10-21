62a4dc4f547a5929cd3119fd640a6060a1000820
TamasSallai <tamas.sallai.hu@gmail.com>
2022-10-21 20:44:36 +0200

    added eslint

--- phonebook-backend/server.js
+++ phonebook-backend/server.js
@@ -8,7 +8,7 @@ const cors = require('cors')
 app.use(express.static('build'))
 app.use(cors())
 app.use(express.json())
-morgan.token('body', (req, res) => JSON.stringify(req.body))
+morgan.token('body', (req) => JSON.stringify(req.body))
 app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms :body')
 )
